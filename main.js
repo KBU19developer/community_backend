9c2374f53039f3a0c23f4c43973271d024bf9c49
hwan <rlaghks1103@gmail.com>
2023-03-03 18:54:05 +0900

    add test html code, error page

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ const index = require('./Routes/index');
 const signup = require('./Routes/Account');
 const uploadImg = require('./Routes/Image');
 const Board = require('./Routes/BoardInfo');
-const post = require('./Routes/Post');
+const AiPage = require('./Routes/AiRoutes');
+//const post = require('./Routes/Post');
 
 
 const app = express();
@@ -17,6 +18,12 @@ const app = express();
 global.logger || (global.logger = require('./config/logger')); //전역에 사용
 const morganMiddleware = require('./config/morganMiddleware');
 app.use(morganMiddleware);
+
+//test html code 
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
 
 const port = 3001; // This is the port number 
 app.use(cors()); // for interacting frontend using API
@@ -27,8 +34,15 @@ app.use('/', index); // index page
 app.use('/Account', signup); // sign up page
 app.use('/IMG', uploadImg); // AI image upload page
 app.use('/Board', Board);
-app.use('/Post', post);
+app.use('/AIPage', AiPage);
+//app.use('/Post', post);
 app.use('*', index); // when search using URL
+
+
+//error page
+app.use((req, res) => {
+    res.status(404).render('404');
+})
 
 app.listen(port, () => {
     console.log(`The server Runing at ${port}`); // Be shown on the CLI when server running 

e608df9b1698335f803f23e7988889e56e96b4d1
hwan <rlaghks1103@gmail.com>
2023-03-03 18:52:12 +0900

    add to UUID, file path

--- Routes/Image.js
+++ Routes/Image.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const DateNow = require('../API/time.js');
+const v4 = require('uuid');
+const Hashing = require('../API/encryt.js');
 
 const storage = multer.diskStorage({destination: (req,file,cb) => { cb(null, path.join(__dirname, '../downloadIMG/')) }, filename: (req, file, cb) => { 
     cb(null, file.fieldname + '-' + DateNow.Time)
@@ -13,7 +15,12 @@ const storage = multer.diskStorage({destination: (req,file,cb) => { cb(null, pat
 router.post('/', upload.single('ImgFile'), function(req,res){ // when receieved image file
     console.log(req.file);
     console.log(req.file.filename);
-    res.send({'response' : 'Hello'});
+    res.send({'response' : 'ok', 'UUID' : v4(), 'path' : Hashing.Hash(storage)});
 });
+
+//test html render 
+router.get('/', (req, res, next) => {
+	res.render('AI_test.html');
+})
 
 module.exports = router;
